4788e3725cfd80372af7d5df93c0911b5ddff24d
PatrickJS <github@gdi2290.com>
2016-06-19 16:58:10 -0700

    feat: allow different classes for ui/container

--- src/debug.ts
+++ src/debug.ts
@@ -1,19 +1,20 @@
-import { Component, ViewEncapsulation, Input } from '@angular/core';
+import { Component, ViewEncapsulation, Input, Attribute } from '@angular/core';
 
 @Component({
   selector: 'ac-debug',
+  encapsulation: ViewEncapsulation.None,
   styles: [`
     .ac-ui.ac-active {
       outline-color: red;
       outline-style: solid;
       transform: translateZ(0);
-    } 
-    
+    }
+
     .ac-container.ac-active {
       outline-color: blue;
       outline-style: solid;
-      transform: translateZ(0);      
-    } 
+      transform: translateZ(0);
+    }
 
     .ac-debug-container {
       width: 350px;
@@ -22,7 +23,6 @@ import { Component, ViewEncapsulation, Input } from '@angular/core';
       margin-top: 100px;
     }
   `],
-  encapsulation: ViewEncapsulation.None,
   template: `
     <div class="ac-debug-container row between-xs" [ngStyle]="getStyles()">
       <button (click)="toggleUi()">toggle ui</button>
@@ -32,34 +32,48 @@ import { Component, ViewEncapsulation, Input } from '@angular/core';
 })
 export class AcDebug {
   @Input() position: string = 'top-left';
+  @Input() acUiClass: string = 'ac-ui';
+  @Input() acContainerClass: string = 'ac-container';
+  @Input() acActiveClass: string = 'ac-active';
 
-  _getPosition(style) {
+  constructor(
+    @Attribute('ac-ui-class') acUiClass?: string,
+    @Attribute('ac-container-class') acContainerClass?: string,
+    @Attribute('ac-active-class') acActiveClass?: string) {
+    this.acUiClass = acUiClass || this.acUiClass;
+    this.acContainerClass = acContainerClass || this.acContainerClass;
+    this.acActiveClass = acActiveClass || this.acActiveClass;
 
+  }
+
+  get containers(): any[] {
+    return Array.from(document.querySelectorAll('.' + this.acContainerClass));
+  }
+
+  get ui(): any[] {
+    return Array.from(document.querySelectorAll('.' + this.acUiClass));
+  }
+
+  getStyles() {
+    return this._getPosition({
+      position: 'absolute'
+    });
+  }
+
+  toggleUi() {
+    this.ui.forEach(ui => ui.classList.toggle(this.acActiveClass));
+  }
+
+  toggleContainer() {
+    this.containers.forEach(container => container.classList.toggle(this.acActiveClass));
+  }
+
+  private _getPosition(style) {
     if (this.position === 'top-left') {
       return Object.assign(style, {
         left: '0',
         top: '0'
       });
     }
-  }
-  getStyles() {
-    return this._getPosition({
-      position: 'absolute'
-    });
-  }
-  get containers(): any[] {
-    return Array.from(document.querySelectorAll('.ac-container'));
-  }
-
-  get ui(): any[] {
-    return Array.from(document.querySelectorAll('.ac-ui'));
-  }
-
-  toggleUi() {
-    this.ui.forEach(ui => ui.classList.toggle('ac-active'));
-  }
-
-  toggleContainer() {
-    this.containers.forEach(container => container.classList.toggle('ac-active'));
   }
 }
